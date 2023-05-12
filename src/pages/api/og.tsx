5aff33f41f5e9b457ed6a3aebddd1b0bbb9c0e84
Andrew Ngo <andrewngorain@gmail.com>
2023-05-12 17:18:27 -0400

    Upgrade next and add open graph image

---
+++ src/pages/api/og.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from '@vercel/og'
+
+export const config = {
+  runtime: 'edge',
+}
+
+const image = fetch(new URL('./open-graph.png', import.meta.url)).then((res) => res.arrayBuffer())
+
+export default async function handler() {
+  const imageData = await image
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          background: '#f6f6f6',
+          width: '100%',
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img width="1920" height="1078" src={imageData} />
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  )
+}
